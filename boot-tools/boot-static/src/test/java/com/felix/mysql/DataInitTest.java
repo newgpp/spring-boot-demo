017b6d73feaeb6733b26b5f77be46016aae0505a
felix <zhaolishan@aipark.com>
2023-12-21 15:55:30 +0800

    增加order初始化

--- boot-tools/boot-static/src/test/java/com/felix/mysql/DataInitTest.java
+++ boot-tools/boot-static/src/test/java/com/felix/mysql/DataInitTest.java
@@ -9,39 +9,19 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.stream.Stream;
 
-/**
- * @author felix
- * <p>
- * CREATE DATABASE IF NOT EXISTS `oper_db`;
- * <p>
- * CREATE TABLE IF NOT EXISTS `sys_oper_log` (
- * `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
- * `oper_id` BIGINT(20) NULL COMMENT '日志主键',
- * `title` VARCHAR(50) NULL COMMENT '模块标题',
- * `business_type` INT(2) NULL DEFAULT '0' COMMENT '业务类型（0其它 1新增 2修改 3删除）',
- * `method` VARCHAR(100) NULL COMMENT '方法名称',
- * `request_method` VARCHAR(10) NULL COMMENT '请求方式',
- * `operator_type` INT(1) NULL DEFAULT '0' COMMENT '操作类别（0其它 1后台用户 2手机端用户）',
- * `oper_name` VARCHAR(50) NULL COMMENT '操作人员',
- * `dept_name` VARCHAR(50) NULL COMMENT '部门名称',
- * `oper_url` VARCHAR(255) NULL COMMENT '请求URL',
- * `oper_ip` VARCHAR(50) NULL COMMENT '主机地址',
- * `oper_location` VARCHAR(255) NULL COMMENT '操作地点',
- * `oper_param` VARCHAR(2000) NULL COMMENT '请求参数',
- * `status` INT(1) NULL DEFAULT '0' COMMENT '操作状态（0正常 1异常）',
- * `error_msg` VARCHAR(2000) NULL COMMENT '错误消息',
- * `oper_time` DATETIME NULL COMMENT '操作时间',
- * `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
- * PRIMARY KEY (`id`)
- * ) COMMENT '操作日志记录';
- */
-public class DbUtilsTest {
+
+public class DataInitTest {
 
     private static final String jdbcUrl = "jdbc:mysql://192.168.159.111:3306/oper_db?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&serverTimezone=Asia/Shanghai";
     private static final String driverClass = "com.mysql.cj.jdbc.Driver";
@@ -60,14 +40,24 @@ public class DbUtilsTest {
         queryRunner = new QueryRunner(dataSource);
     }
 
-    private Object randomGet(Object[] arr) {
-        int i = new Random().nextInt(Integer.MAX_VALUE);
-        int index = i % arr.length;
-        return arr[index];
+    @Test
+    public void query_oper_log_should_success() {
+        //given
+        String sql = "select * from sys_oper_log where id in (1, 2)";
+        //when
+        try {
+            List<Map<String, Object>> list = queryRunner.query(sql, new MapListHandler());
+            //then
+            for (Map<String, Object> line : list) {
+                System.out.println(JSON.toJSONString(line));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void insert_data_should_success() {
+    public void insert_oper_log_should_success() {
         //given
         String sql = "insert into sys_oper_log(oper_id, title, business_type, method, request_method, operator_type, oper_name, dept_name, oper_url, oper_ip, oper_location, oper_param, status, error_msg, oper_time) values" +
                 "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -85,13 +75,12 @@ public class DbUtilsTest {
                 "[1,2]", "{\"name\":\"felix\", \"orderId\":123}", "", "{}", "userId=1&unread=0", "userId=1&carId=&page=1&pageSize=15&startTime=2023-12-19%2013%3A53%3A11&t=1702965187202"};
         Object[] statuss = {0, 1};
         Object[] error_msgs = {"", null, "系统错误", "Error:NullPointerException", "success"};
-        Object[] oper_times = {"2023-12-19 00:10:09", "2023-09-20 17:59:58", "2023-10-19 00:10:09", "2023-03-19 12:05:09", "2023-04-20 14:19:23", "2023-07-10 18:09:46"};
         //when
-        Stream.iterate(0, x -> x + 1).limit(10).forEach(x -> {
+        Stream.iterate(0, x -> x + 1).limit(5).forEach(x -> {
             try {
                 Object[] params = new Object[]{IdUtils.nextSnowflakeId(), randomGet(titles), randomGet(business_types), randomGet(methods), randomGet(request_methods), randomGet(operator_types)
                         , randomGet(oper_names), randomGet(dept_names), randomGet(oper_urls), randomGet(oper_ips), randomGet(oper_locations), randomGet(oper_params), randomGet(statuss)
-                        , randomGet(error_msgs), randomGet(oper_times)};
+                        , randomGet(error_msgs), dateRandom("2023-12-01 00:00:00", "2023-12-21 00:00:00")};
                 Object insert = queryRunner.insert(sql, new ScalarHandler<>(), params);
                 System.out.println(insert);
                 //then
@@ -102,19 +91,66 @@ public class DbUtilsTest {
     }
 
     @Test
-    public void query_list_should_success() {
+    public void insert_t_order_should_success() {
         //given
-        String sql = "select * from sys_oper_log where id in (1, 2)";
+        String sql = "insert into t_order (order_no, customer_id, order_time, order_status, order_amount, goods_id) values "
+                + "(?, ?, ?, ?, ?, ?)";
+
+        Object[] order_statuses = {0, 1, 2, 3};
+        Object[] goods_ids = Stream.iterate(0, x -> x + 1).limit(100).toArray();
+        Object[] customer_ids = Stream.iterate(1000, x -> x + 1).limit(10).toArray();
+
         //when
-        try {
-            List<Map<String, Object>> list = queryRunner.query(sql, new MapListHandler());
-            //then
-            for (Map<String, Object> line : list) {
-                System.out.println(JSON.toJSONString(line));
+        Stream.iterate(0, x -> x + 1).limit(5).forEach(x -> {
+            try {
+                long order_no = IdUtils.nextSnowflakeId();
+                Object customer_id = randomGet(customer_ids);
+                String order_time = dateRandom("2023-12-01 00:00:00", "2023-12-21 00:00:00");
+                Object order_status = randomGet(order_statuses);
+                String order_amount = moneyRandom("10", "500");
+                Object goods_id = randomGet(goods_ids);
+                Object[] params = new Object[]{order_no, customer_id, order_time, order_status, order_amount, goods_id};
+                Object insert = queryRunner.insert(sql, new ScalarHandler<>(), params);
+                System.out.println(insert);
+                //then
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
     }
 
+    private Object randomGet(Object[] arr) {
+        int i = new Random().nextInt(Integer.MAX_VALUE);
+        int index = i % arr.length;
+        return arr[index];
+    }
+
+    private Long rangeRandom(long min, long max) {
+        long bound = max - min;
+        int i = new Random().nextInt((int) bound);
+        return min + i;
+    }
+
+    private String dateRandom(String start, String end) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        long min = 0;
+        long max = 0;
+        try {
+            min = format.parse(start).getTime();
+            max = format.parse(end).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Long l = rangeRandom(min, max);
+        Date date = new Date(l);
+        return format.format(date);
+    }
+
+
+    private String moneyRandom(String min, String max) {
+        String lMin = new BigDecimal(min).multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP).toPlainString();
+        String lMax = new BigDecimal(max).multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP).toPlainString();
+        Long num = rangeRandom(Long.parseLong(lMin), Long.parseLong(lMax));
+        return new BigDecimal(num).divide(new BigDecimal(100), 2, RoundingMode.HALF_UP).toPlainString();
+    }
 }
